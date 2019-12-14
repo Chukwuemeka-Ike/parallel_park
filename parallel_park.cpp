f0e377cebe4a9c3b095d513231e6e293a845eb45
Chukwuemeka O Ike <chukwuemeka.o.ike@gmail.com>
2019-12-14 10:54:17 -0500

    First

--- parallel_park.cpp
+++ parallel_park.cpp
@@ -118,13 +118,13 @@ int main(int argc, char **argv){
 			control.throttle = (float) (-0.65);
 			pub.publish(control);
 			ROS_INFO_STREAM("Right turn");
-			ros::Duration(1).sleep();
+			ros::Duration(0.9).sleep();
 
 			control.angle = (float) (0.9);
 			control.throttle = (float) (-0.65);
 			pub.publish(control);
 			ROS_INFO_STREAM("Left turn");
-			ros::Duration(0.5).sleep();
+			ros::Duration(0.45).sleep();
 
 			control.angle = (float) (-0.9);
 			control.throttle = (float) (0.65);
